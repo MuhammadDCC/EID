85e942490b344bd91e0a8776f0b2821e42822d04
MuhammadDCC <104708160+MuhammadDCC@users.noreply.github.com>
2022-05-01 15:19:34 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = prompt('Enter your Nickname  😋');
+let b= document.getElementById('pa')
+
+if(a){
+   b.innerText = "Hey "+a+"..."
+  
+}
+else{
+   alert("You didn't give your name 😟");
+}
